3eef87e267078a032334c1e4be4e3bc696a09cc1
Richard Zou <zou3519@gmail.com>
2018-07-26 11:49:28 -0700

    Put the version string back

--- docs/stable/_static/documentation_options.js
+++ docs/stable/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'stable',
+    VERSION: 'master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
