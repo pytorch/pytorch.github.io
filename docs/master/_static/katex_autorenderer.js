92923a7c70759bfcf7194af2b15c4339e46cabe3
pytorchbot <soumith+bot@pytorch.org>
2018-09-06 14:18:55 +0000

    auto-generating sphinx docs

--- docs/master/_static/katex_autorenderer.js
+++ docs/master/_static/katex_autorenderer.js
@@ -1,18 +1,12 @@
 katex_options = {
-
 delimiters : [
    {left: "$$", right: "$$", display: true},
    {left: "\\(", right: "\\)", display: true},
    {left: "\\[", right: "\\]", display: true}
 ],
-strict : false
-,
-delimiters: [
-        { left: "\\(", right: "\\)", display: false },
-        { left: "\\[", right: "\\]", display: true }
-        ],
-}
+strict : false,
 
+}
 document.addEventListener("DOMContentLoaded", function() {
   renderMathInElement(document.body, katex_options);
 });
