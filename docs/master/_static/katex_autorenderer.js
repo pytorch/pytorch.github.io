ee18e7becaef6cc8fbb2d010934186380d089907
pytorchbot <soumith+bot@pytorch.org>
2018-09-10 20:33:52 +0000

    auto-generating sphinx docs

--- docs/master/_static/katex_autorenderer.js
+++ docs/master/_static/katex_autorenderer.js
@@ -1,10 +1,9 @@
 katex_options = {
 delimiters : [
    {left: "$$", right: "$$", display: true},
-   {left: "\\(", right: "\\)", display: true},
+   {left: "\\(", right: "\\)", display: false},
    {left: "\\[", right: "\\]", display: true}
 ],
-strict : false,
 
 }
 document.addEventListener("DOMContentLoaded", function() {
