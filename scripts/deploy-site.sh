62f30a6bee7c317353633096572a4e59c26034f0
Soumith Chintala <soumith@gmail.com>
2019-06-01 15:02:40 -0400

    Revert "update hub upon build_deploy"
    This reverts commit 25ed85dda37378287e288fc56ef24d36cf3c6580.

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -29,8 +29,12 @@ else
     git checkout --orphan master
 fi
 
+# Update submodule pytorch/hub to the latest master branch
+# Then copy over images to assets/images
+git submodule update --remote _hub
+cp _hub/images/* assets/images
+
 cd "../"
-make update-hub
 make build_deploy
 cd master-branch
 
