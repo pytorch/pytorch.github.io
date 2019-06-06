041586f39dc9f03ae69853e1ab527fdac946595d
Soumith Chintala <soumith@gmail.com>
2019-06-06 18:01:13 -0400

    fix shell script

--- _devel/update_hub_submodule.sh
+++ _devel/update_hub_submodule.sh
@@ -8,7 +8,7 @@ pip install notedown
 mkdir -p assets/hub/
 
 pushd _hub
-find . -type f -name '*.md' -depth 1 | grep -v "README" | cut -f2- -d"/" |
+find . -maxdepth 1 -name "*.md" | grep -v "README" | cut -f2- -d"/" |
     while read file; do
         tail -n +15 "$file" | notedown >"../assets/hub/${file%.md}.ipynb";
     done
