687f255daf8d97c33de0151847132e1fac04602d
Soumith Chintala <soumith@gmail.com>
2019-06-06 17:30:28 -0400

    add google colab links

--- _devel/update_hub_submodule.sh
+++ _devel/update_hub_submodule.sh
@@ -1,4 +1,15 @@
+set -ex
 pushd _hub
 git pull https://github.com/pytorch/hub
 popd
 cp _hub/images/* assets/images/
+
+pip install notedown
+mkdir -p assets/hub/
+
+pushd _hub
+find . -type f -name '*.md' -depth 1 | grep -v "README" | cut -f2- -d"/" |
+    while read file; do
+        tail -n +15 "$file" | notedown >"../assets/hub/${file%.md}.ipynb";
+    done
+popd
