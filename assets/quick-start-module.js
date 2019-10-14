015c91450ab908dc7964f9b1cb9e2d777c51acc1
peterjc123 <peterghost86@gmail.com>
2019-10-14 16:49:36 +0800

    Fix typos

--- assets/quick-start-module.js
+++ assets/quick-start-module.js
@@ -514,13 +514,13 @@ function commandMessage(key) {
       "conda install pytorch torchvision cpuonly -c pytorch-nightly -c defaults -c conda-forge",
 
     "preview,conda,windows,cuda9.2,python3.7":
-      "conda install pytorch torchvision cudatoolkit=9.2 -c pytorch-nightly -c defaults -c defaults -c numba/label/dev",
+      "conda install pytorch torchvision cudatoolkit=9.2 -c pytorch-nightly -c defaults -c conda-forge -c numba/label/dev",
 
     "preview,conda,windows,cuda10.1,python3.7":
-      "conda install pytorch torchvision cudatoolkit=10.1 -c pytorch-nightly -c defaults -c defaults",
+      "conda install pytorch torchvision cudatoolkit=10.1 -c pytorch-nightly -c defaults -c conda-forge",
 
     "preview,conda,windows,cudanone,python3.7":
-      "conda install pytorch torchvision cpuonly -c pytorch-nightly -c defaults -c defaults",
+      "conda install pytorch torchvision cpuonly -c pytorch-nightly -c defaults -c conda-forge",
 
     "preview,pip,macos,cuda9.2,python2.7":
       "pip install numpy<br />pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html<br /># On MacOS, we provide CPU-only packages, CUDA functionality is not provided",
