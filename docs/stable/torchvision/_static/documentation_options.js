667354311c6ec91d7369a336a4cd890e7c30a626
Website Deployment Script <facebook-circleci-bot@users.noreply.github.com>
2020-10-27 19:01:04 +0000

    Deploy to GitHub Pages on master [ci skip]

--- docs/stable/torchvision/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.8.0.dev20201012',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
