bbbd944d57c0ae6830e39bc0ffe67e5657e5adc7
Website Deployment Script <facebook-circleci-bot@users.noreply.github.com>
2021-02-22 19:36:40 +0000

    Deploy to GitHub Pages on master [ci skip]

--- assets/quick-start-module.js
+++ assets/quick-start-module.js
@@ -240,7 +240,7 @@ function commandMessage(key) {
       "pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html",
 
     "stable,pip,linux,cuda10.2,python":
-      "pip install torch torchvision",
+      "pip install torch torchvision torchaudio",
 
     "stable,pip,linux,cuda11.0,python":
       "pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html",
