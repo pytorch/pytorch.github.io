59839d48cea70759d6dd10fb059994ac3bf7695d
atalman <atalman@fb.com>
2022-11-28 12:32:34 -0800

    Address comments

--- scripts/gen_quick_start_module.py
+++ scripts/gen_quick_start_module.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from typing import Dict, Set, List, Iterable
 from enum import Enum
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR =  Path(__file__).parent.parent
 
 class OperatingSystem(Enum):
     LINUX: str = "linux"
@@ -50,43 +50,52 @@ LIBTORCH_DWNL_INSTR = {
         DEBUG: "Download here (Debug version):",
     }
 
+def load_json_from_basedir(filename: str):
+    try:
+        with open(BASE_DIR / filename) as fp:
+            return json.load(fp)
+    except FileNotFoundError as e:
+        raise ImportError(f"File {filname} not found error: {e.strerror}") from e
+    except json.JSONDecodeError as e:
+        raise ImportError(f"Invalid JSON {filname} error: {e.strerror}") from e
+
 def read_published_versions():
-    with open(os.path.join(BASE_DIR, "published_versions.json")) as fp:
-        return json.load(fp)
+    return load_json_from_basedir("published_versions.json")
 
 def write_published_versions(versions):
-    with open(os.path.join(BASE_DIR, "published_versions.json"), "w") as outfile:
+    with open(BASE_DIR / "published_versions.json", "w") as outfile:
             json.dump(versions, outfile, indent=2)
 
 def read_matrix_for_os(osys: OperatingSystem, value: str):
-    try:
-        with open(os.path.join(BASE_DIR, f"{osys.value}_{value}_matrix.json")) as fp:
-            return json.load(fp)["include"]
-    except FileNotFoundError as e:
-        raise ImportError(f"Release matrix not found for: {osys.value} error: {e.strerror}") from e
-
+    jsonfile = load_json_from_basedir(f"{osys.value}_{value}_matrix.json")
+    return jsonfile["include"]
 
 def read_quick_start_module_template():
-    with open(os.path.join(BASE_DIR, "_includes", "quick-start-module.js")) as fp:
+    with open(BASE_DIR / "_includes" / "quick-start-module.js") as fp:
         return fp.read()
 
+def get_package_type(pkg_key, os_key):
+    package_type = pkg_key
+    if pkg_key == "pip":
+        package_type = "manywheel" if os_key == OperatingSystem.LINUX.value else "wheel"
+    return package_type
+
+def get_gpu_info(acc_key, instr, acc_arch_map):
+    gpu_arch_type, gpu_arch_version = acc_arch_map[acc_key]
+    if DEFAULT in instr:
+        gpu_arch_type, gpu_arch_version = acc_arch_map["accnone"]
+    return (gpu_arch_type, gpu_arch_version)
+
+# This method is used for generating new published_versions.json file
+# It will modify versions json object with installation instructions
+# Provided by generate install matrix Github Workflow, stored in release_matrix
+# json object.
 def update_versions(versions, release_matrix, release_version):
-    version_map = {
-        "preview": "preview",
-    }
-    version = ""
+    version = "preview"
     acc_arch_map = acc_arch_ver_map[release_version]
 
-    if(release_version == "nightly"):
-        version = "preview"
-    else:
+    if(release_version != "nightly"):
         version = release_matrix[OperatingSystem.LINUX.value][0]["stable_version"]
-
-    # Generating for a specific version
-    if(version != "preview"):
-        version_map = {
-            version: version,
-        }
         if version not in versions["versions"]:
             import copy
             new_version = copy.deepcopy(versions["versions"]["preview"])
@@ -94,58 +103,46 @@ def update_versions(versions, release_matrix, release_version):
             versions["latest_stable"] = version
 
     # Perform update of the json file from release matrix
-    for ver, ver_key in version_map.items():
-        for os_key, os_vers in versions["versions"][ver_key].items():
-            for pkg_key, pkg_vers in os_vers.items():
-                for acc_key, instr in pkg_vers.items():
+    for os_key, os_vers in versions["versions"][version].items():
+        for pkg_key, pkg_vers in os_vers.items():
+            for acc_key, instr in pkg_vers.items():
+                package_type = get_package_type(pkg_key, os_key)
+                gpu_arch_type, gpu_arch_version = get_gpu_info(acc_key, instr, acc_arch_map)
 
-                    package_type = pkg_key
-                    if pkg_key == 'pip':
-                        package_type = 'manywheel' if os_key == OperatingSystem.LINUX.value else 'wheel'
+                pkg_arch_matrix = [
+                    x for x in release_matrix[os_key]
+                    if (x["package_type"], x["gpu_arch_type"], x["gpu_arch_version"]) ==
+                    (package_type, gpu_arch_type, gpu_arch_version)
+                    ] or None
 
-                    gpu_arch_type, gpu_arch_version = acc_arch_map[acc_key]
-                    if(DEFAULT in instr):
-                        gpu_arch_type, gpu_arch_version = acc_arch_map["accnone"]
-
-                    pkg_arch_matrix = list(filter(
-                            lambda x:
-                            (x["package_type"], x["gpu_arch_type"], x["gpu_arch_version"]) ==
-                            (package_type, gpu_arch_type, gpu_arch_version),
-                            release_matrix[os_key]
-                        ))
-
-                    if pkg_arch_matrix:
-                        if package_type != 'libtorch':
-                            instr["command"] = pkg_arch_matrix[0]["installation"]
-                        else:
-                            if os_key == OperatingSystem.LINUX.value:
-                                rel_entry_pre_cxx1 = next(filter(
-                                    lambda x:
-                                    x["devtoolset"] == PRE_CXX11_ABI,
-                                    pkg_arch_matrix
-                                ), None)
-                                rel_entry_cxx1_abi = next(filter(
-                                    lambda x:
-                                    x["devtoolset"] == CXX11_ABI,
-                                    pkg_arch_matrix
-                                ), None)
-                                if(instr['versions'] is not None):
-                                    instr['versions'][LIBTORCH_DWNL_INSTR[PRE_CXX11_ABI]] = rel_entry_pre_cxx1["installation"]
-                                    instr['versions'][LIBTORCH_DWNL_INSTR[CXX11_ABI]] = rel_entry_cxx1_abi["installation"]
-                            elif os_key == OperatingSystem.WINDOWS.value:
-                                rel_entry_release = next(filter(
-                                    lambda x:
-                                    x["libtorch_config"] == RELEASE,
-                                    pkg_arch_matrix
-                                ), None)
-                                rel_entry_debug = next(filter(
-                                    lambda x:
-                                    x["libtorch_config"] == DEBUG,
-                                    pkg_arch_matrix
-                                ), None)
-                                if(instr['versions'] is not None):
-                                    instr['versions'][LIBTORCH_DWNL_INSTR[RELEASE]] = rel_entry_release["installation"]
-                                    instr['versions'][LIBTORCH_DWNL_INSTR[DEBUG]] = rel_entry_debug["installation"]
+                if pkg_arch_matrix is not None:
+                    if package_type != "libtorch":
+                        instr["command"] = pkg_arch_matrix[0]["installation"]
+                    else:
+                        if os_key == OperatingSystem.LINUX.value:
+                            rel_entry_pre_cxx1 = next(
+                                x for x in pkg_arch_matrix
+                                if x["devtoolset"] == PRE_CXX11_ABI
+                             )
+                            rel_entry_cxx1_abi = next(
+                                x for x in pkg_arch_matrix
+                                if x["devtoolset"] == CXX11_ABI
+                                )
+                            if(instr["versions"] is not None):
+                                instr["versions"][LIBTORCH_DWNL_INSTR[PRE_CXX11_ABI]] = rel_entry_pre_cxx1["installation"]
+                                instr["versions"][LIBTORCH_DWNL_INSTR[CXX11_ABI]] = rel_entry_cxx1_abi["installation"]
+                        elif os_key == OperatingSystem.WINDOWS.value:
+                            rel_entry_release = next(
+                                x for x in pkg_arch_matrix
+                                if x["libtorch_config"] == RELEASE
+                                )
+                            rel_entry_debug = next(
+                                x for x in pkg_arch_matrix
+                                if x["libtorch_config"] == DEBUG
+                                )
+                            if(instr["versions"] is not None):
+                                instr["versions"][LIBTORCH_DWNL_INSTR[RELEASE]] = rel_entry_release["installation"]
+                                instr["versions"][LIBTORCH_DWNL_INSTR[DEBUG]] = rel_entry_debug["installation"]
 
 
 def gen_install_matrix(versions) -> Dict[str, str]:
@@ -175,19 +172,13 @@ def gen_install_matrix(versions) -> Dict[str, str]:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--autogenerate",
-        help="Is this call being initiated from workflow? update published_versions",
-        type=str,
-        choices=[ENABLE, DISABLE],
-        default=ENABLE,
-    )
+    parser.add_argument('--autogenerate', dest='autogenerate', action='store_true')
+    parser.set_defaults(autogenerate=False)
 
     options = parser.parse_args()
     versions = read_published_versions()
 
-
-    if options.autogenerate == ENABLE:
+    if options.autogenerate:
         release_matrix = {}
         for val in ("nightly", "release"):
             release_matrix[val] = {}
