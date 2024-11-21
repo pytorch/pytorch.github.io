e902e2b4a68394c772427682da9547c8a12e9904
atalman <atalman@fb.com>
2024-11-21 07:31:59 -0800

    undo_rocm_change

--- assets/quick-start-module.js
+++ assets/quick-start-module.js
@@ -11,7 +11,7 @@ var archInfoMap = new Map([
   ['accnone', {title: "CPU", platforms: new Set(['linux', 'macos', 'windows'])}]
 ]);
 
-let version_map={"nightly": {"accnone": ["cpu", ""], "cuda.x": ["cuda", "11.8"], "cuda.y": ["cuda", "12.1"], "cuda.z": ["cuda", "12.4"], "rocm5.x": ["rocm", "6.0"]}, "release": {"accnone": ["cpu", ""], "cuda.x": ["cuda", "11.8"], "cuda.y": ["cuda", "12.1"], "cuda.z": ["cuda", "12.4"], "rocm5.x": ["rocm", "6.0"]}}
+let version_map={"nightly": {"accnone": ["cpu", ""], "cuda.x": ["cuda", "11.8"], "cuda.y": ["cuda", "12.1"], "cuda.z": ["cuda", "12.4"], "rocm5.x": ["rocm", "6.2"]}, "release": {"accnone": ["cpu", ""], "cuda.x": ["cuda", "11.8"], "cuda.y": ["cuda", "12.1"], "cuda.z": ["cuda", "12.4"], "rocm5.x": ["rocm", "6.2"]}}
 let stable_version="Stable (2.5.1)";
 
 var default_selected_os = getAnchorSelectedOS() || getDefaultSelectedOS();
@@ -281,4 +281,3 @@ function commandMessage(key) {
 
 // Set cuda version right away
 changeVersion("stable")
-
